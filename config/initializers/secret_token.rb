f1bb59099058ddaf474e02c310a50c0d240e202f
TammyLLC <me@TammyLLC.com>
2014-02-19 08:38:42 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyTTS219::Application.config.secret_key_base = '2ec41318e93936023a4a8f2e64ed7102b79c0a5c907fa6dc18f2b242aebe28995da62bd48267ef56072fb0ebf6a59904e7e179a64a1e03a366d0f78f893363ca'
